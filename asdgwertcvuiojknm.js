fba2219798de236c7542e71e31fe63c4e7088527
Code <Code>
2020-01-19 02:34:46 +0800

    nothing

--- asdgwertcvuiojknm.js
+++ asdgwertcvuiojknm.js
@@ -34,6 +34,9 @@ imap('jj', "<Esc>");  // 按两次j退出当前输入框
 //map('o', 'go')
 //map('F', 'gf')
 
+// 在gmail.com、twitter.com只保留 E、R和T
+unmapAllExcept(['E','R','T'], /gmail.com|twitter.com/);
+
 
 // #快捷键访问指定网站
 /* 模板
@@ -70,6 +73,41 @@ mapkey(',z', '#120Open 人人影视字幕组', function () {
 });
 mapkey(',e', '#120Open Chrome Extensions', function () {
     tabOpenLink("chrome://extensions/");
+});
+
+
+// 注册内联查询
+Front.registerInlineQuery({
+    url: "https://api.shanbay.com/bdc/search/?word=",
+    parseResult: function(res) {
+        try {
+            res = JSON.parse(res.text);
+            var exp = res.msg;
+            if (res.data.definition) {
+                var pronunciations = [];
+                for (var reg in res.data.pronunciations) {
+                    pronunciations.push(`<div>[${reg}] ${res.data.pronunciations[reg]}</div>`);
+                    // pronunciations.push(`<div><audio src="${res.data[reg+'_audio']}" controls></audio></div>`);
+                }
+                var definition = res.data.definition.split("\n").map(function(d) {
+                    return `<li>${d}</li>`;
+                }).join("");
+                exp = `${pronunciations.join("")}<ul>${definition}</ul>`;
+            }
+            if (res.data.en_definitions) {
+                exp += "<hr/>";
+                for (var lex in res.data.en_definitions) {
+                    var sense = res.data.en_definitions[lex].map(function(s) {
+                        return `<li>${s}</li>`;
+                    }).join("");
+                    exp += `<div>${lex}</div><ul>${sense}</ul>`;
+                }
+            }
+            return exp;
+        } catch (e) {
+            return "";
+        }
+    }
 });
 
 
